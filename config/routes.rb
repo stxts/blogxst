2c71d5b1eacde93ee59f9ae40060354ab7c638b4
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2016-08-31 00:22:30 -0700

    added CRUD, bacomments, and sic http authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root 'articles#index'
+  resources  :articles do
+    resources  :comments
+  end
+end
+  #root 'welcome#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +61,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
